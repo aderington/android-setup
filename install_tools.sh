0edc3bf65eba1149098f48d6ab942e60dbc8d05d
Angella Derington <angella.derington@gmail.com>
2024-12-26 13:33:59 -0800

    Update install_tools.sh

--- install_tools.sh
+++ install_tools.sh
@@ -1,46 +1,72 @@
 #!/bin/zsh
 
+# Update PATH for given package
+update_path() {
+  echo "Updating PATH to include $1..."
+  
+  # Create or update the .zshrc file with the aliases
+  cat <<EOL>> ~/.zshrc
+
+  # Add $1 executables to PATH
+  export PATH="$2:\$PATH"
+
+EOL
+# Do NOT update the indentation of 'EOL' as the script will fail if 'EOL' has any indentation
+
+  source ~/.zshrc
+  cat ~/.zshrc
+  echo $PATH
+  echo ".zshrc has been updated and changes applied."
+
+
+}
+
 # Function to install Homebrew on macOS
 install_homebrew() {
   if ! command -v brew &> /dev/null; then
     echo "Installing Homebrew..."
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+  # Update path to include brew
+  update_path brew /usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin
+
   fi
 }
 
 # Function to install Git, GitHub CLI, Gradle, and Android Studio
 install_packages() {
   echo "Installing Git..."
-  brew install git
+  /usr/bin/brew install git
   
   echo "Installing GitHub CLI..."
-  brew install gh
+  /usr/bin/brew install gh
 
   echo "Installing Java 11..."
-  brew install openjdk@11 
+  /usr/bin/brew install openjdk@11 
 
   echo "Installing Java 17..."
-  brew install openjdk@17 
+  /usr/bin/brew install openjdk@17 
   
   echo "Installing Gradle..."
-  brew install gradle
+  /usr/bin/brew install gradle
   
   echo "Installing Android Studio..."
-  brew install --cask android-studio
+  /usr/bin/brew install --cask android-studio
+
+  update_path android-studio ~/Android/sdk/platform-tools
+  update_path gradle /usr/local/opt/gradle/bin
 }
 
 # Function to update .zshrc with aliases
 update_zshrc() {
   echo "Updating .zshrc with common aliases..."
+  set +x
   
   # Create or update the .zshrc file with the aliases
-  cat <<EOL >> ~/.zshrc
-
-  # Add commands to PATH
-  export PATH="$PATH:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:~/Android/sdk/platform-tools:/usr/local/opt/gradle/bin"
+  cat <<EOL>> ~/.zshrc
 
   # Set Java to default to v17
-  export JAVA_HOME=$(/usr/libexec/java_home -v 17)
+  export JAVA_HOME=\$(/usr/libexec/java_home -v 17)
 
   # Common aliases for Git
   alias g='git'
@@ -63,7 +89,7 @@ update_zshrc() {
   alias gclean='gw clean'
 
   # Common aliases for GitHub
-  alias ghWatch = 'gh run watch'
+  alias ghWatch='gh run watch'
   ghRun() {
     if [ $# -lt 2 ]
     then
@@ -93,13 +119,13 @@ update_zshrc() {
      # Check if in a Git repository
      if git rev-parse --is-inside-work-tree &>/dev/null; then
         # If in a Git repo, set a specific prompt with branch name
-        local branch_name=$(git rev-parse --abbrev-ref HEAD)
+        local branch_name=\$(git rev-parse --abbrev-ref HEAD)
 
         # Set up the prompt (with git branch name)
-        PROMPT="[%1~] %{$fg[green]%}$branch_name%{$reset_color%} %# "
+        PROMPT="[%1~] %{$fg[green]%}\$branch_name%{\$reset_color%} %# "
      else
         # Use default prompt
-        PROMPT="$DEFAULT_PROMPT"
+        PROMPT="\$DEFAULT_PROMPT"
      fi   
   }
   
@@ -112,10 +138,14 @@ update_zshrc() {
   # Automatically update the prompt when the branch changes
   precmd_functions+=(update_prompt)
 
-  EOL
+EOL
+# Do NOT update the indentation of 'EOL' as the script will fail if 'EOL' has any indentation
 
+  set -x
+  echo ".zshrc has been updated and changes applied."
   source ~/.zshrc
   echo ".zshrc updated!"
+  echo $PATH
 }
 
 # Main installation workflow
